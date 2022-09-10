a39dc40167ce106a9400a01dfbd0764d0d48fc0c
Tolulope Ade-ojo <tolexskii@gmail.com>
2022-09-10 00:05:10 -0700

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry, print single digit numbers seperated by comma
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
