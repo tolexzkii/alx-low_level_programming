aae79a07d907788bf28fa307d8fc2302dbd62863
Tolulope Ade-ojo <tolexskii@gmail.com>
2022-09-14 15:47:47 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: number to be added
+ * @b: number to be added
+ *
+ * Return: value of sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
