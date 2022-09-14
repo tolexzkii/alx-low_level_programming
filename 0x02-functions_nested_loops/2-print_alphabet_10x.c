65dec8ee3721cf19dd477a6cf121efe77e5201c9
Tolulope Ade-ojo <tolexskii@gmail.com>
2022-09-13 17:00:11 -0700

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_10x.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * 
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
