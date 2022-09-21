f7bcf5eef3fccaa8cf345dd70f03f7e9abf104a4
Tolulope Ade-ojo <tolexskii@gmail.com>
2022-09-21 13:11:30 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: bytes used from src
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c = 0, c1 = 0;
+
+	while (*(dest + c) != '\0')
+	{
+		c++;
+	}
+
+	while (c1 < n)
+	{
+		*(dest +c) = *(src + c1);
+		if(*(src + c1) == '\0')
+			break;
+		c++;
+		c1++;
+	}
+	return (dest);
+}
