70daeab79f373e2fb1877b8bf7f85d7f64223a48
Tolulope Ade-ojo <tolexskii@gmail.com>
2022-09-22 08:37:30 -0700

    string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string
+ * Return: destination
+ */
+char *string_toupper(char *s)
+{
+	int c = 0;
+
+	while (*(s + c) != '\0')
+	{
+		if ((*(s + c) >= 97) && (*(s + c) <= 122))
+			*(s + c) = *(s + c) - 32;
+		c++;
+	}
+	return (s);
+}
