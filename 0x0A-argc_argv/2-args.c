db65d579b9d03bb0157390283b97455ead995bd6
Tolulope Ade-ojo <tolexskii@gmail.com>
2022-09-30 07:42:08 -0700

    args again

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: argument counter
+ * @argv: array of strings
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
