5cd3d5f5a1e8a2d2ec017941f4678ac4735ca543
Tolulope Ade-ojo <tolexskii@gmail.com>
2022-10-10 08:33:20 -0700

    init dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,9 +2,9 @@
 #define _DOG_H_
 
 /**
- * dog_type - Typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
-typedef struct dog dog_type;
+typedef struct dog dog_t;
 
 /**
  * struct dog - struct for dog information
@@ -19,5 +19,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
