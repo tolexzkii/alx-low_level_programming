39b2897563edb452df87335be04965b230986928
Ade-ojo Tolulope <tolexskii@gmail.com>
2022-10-20 07:27:57 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,6 +14,8 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		n_element++;
