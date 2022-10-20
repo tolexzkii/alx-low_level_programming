38d5f2fdb89fa55fe4fabda204b851960c4de1bd
Ade-ojo Tolulope <tolexskii@gmail.com>
2022-10-20 13:21:22 -0700

    First

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * before_main - function executed before main
+ */
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
